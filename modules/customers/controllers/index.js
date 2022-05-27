1699472e7511c2000fba8bbeb1bfc72cb95895a8
me1o-GLT <hosam@glt-co.com>
2022-05-27 20:38:48 +0200

    fix

--- modules/customers/controllers/index.js
+++ modules/customers/controllers/index.js
@@ -4,7 +4,7 @@
  * @memberof! Customer
  */
 
-module.exports.getById = require('./getinfoById.controller');
+module.exports.getById = require('./getInfoById.controller');
 module.exports.getByPhone = require('./getinfoByPhone.controller');
 module.exports.list = require('./list.controller');
 
