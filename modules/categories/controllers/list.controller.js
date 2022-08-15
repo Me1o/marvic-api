9ad25fe326ef84a9d058bb90e6e1bae30ad8fb5a
me1o-GLT <hossameldien.nasr@zid.sa>
2022-08-15 23:46:36 +0200

    hotfix

--- modules/categories/controllers/list.controller.js
+++ modules/categories/controllers/list.controller.js
@@ -10,10 +10,11 @@ module.exports = async (req, res) => {
   try {
      let userId = req.user.dataValues.id;
      const user = await User.findOne({ where: { id: userId }, attributes: ['storeId'], raw: true });
-     const categories = await Category.findAll({ where: { storeId: user.storeId }, raw: true });
+     var categories = await Category.findAll({ where: { storeId: user.storeId }, raw: true });
      categories.forEach(async(cat) => {
       cat.products = await Product.count({ where: { categoryId: cat.id } });
      });
+
      res.json({ case: 0, message: categories });
 
 
