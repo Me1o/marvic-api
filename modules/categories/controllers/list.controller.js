8bdc438ab94dbc4992bfe0194aa638962db3133e
me1o-GLT <hossameldien.nasr@zid.sa>
2022-08-15 23:38:45 +0200

    category product count

--- modules/categories/controllers/list.controller.js
+++ modules/categories/controllers/list.controller.js
@@ -1,6 +1,6 @@
 const Joi = require('marvic-api/helpers/joi');
 const { Op } = require('sequelize');
-const { models: { User, Category } } = require('marvic-api/helpers/models');
+const { models: { User, Category, Product } } = require('marvic-api/helpers/models');
 
 module.exports = async (req, res) => {
   /**
@@ -11,11 +11,13 @@ module.exports = async (req, res) => {
      let userId = req.user.dataValues.id;
      const user = await User.findOne({ where: { id: userId }, attributes: ['storeId'], raw: true });
      const categories = await Category.findAll({ where: { storeId: user.storeId }, raw: true });
-
+     categories.forEach(async(cat) => {
+      cat.products = await Product.count({ where: { categoryId: cat.id } });
+     });
      res.json({ case: 0, message: categories });
-     
-   
-  
+
+
+
   } catch (err) {
     return res.json({ case: 0, message: 'Something went wrong!', err });
   }
