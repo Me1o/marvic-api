082b271dae71dbb37f3bc21507602e2171a1d9d7
notmhmd <mhmdfrj.97@gmail.com>
2022-09-01 18:38:29 +0200

    sync order modal

--- modules/orders/order.model.js
+++ modules/orders/order.model.js
@@ -9,39 +9,43 @@
  * @memberof Order.Model
  * @param {Object} sequelize - Sequelize object
  * @param {Object} DataTypes - Sequelize data types
-  */
+ */
 module.exports = (sequelize, DataTypes) => {
   const OrderSchema = {
     id: {
       primaryKey: true,
-      type: DataTypes.INTEGER, autoIncrement: true
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
     },
     status: {
       type: DataTypes.INTEGER,
-      defaultValue: 1
+      defaultValue: 1,
     },
-     products: {
-      type: DataTypes.TEXT
-     },
-     customerId: {
-      type: DataTypes.INTEGER
-    }, 
+    products: {
+      type: DataTypes.TEXT,
+    },
+    customerId: {
+      type: DataTypes.INTEGER,
+    },
     storeId: {
-      type: DataTypes.INTEGER
+      type: DataTypes.INTEGER,
     },
     address: {
-      type: DataTypes.TEXT
+      type: DataTypes.TEXT,
     },
     price: {
-      type: DataTypes.INTEGER
-    }
-    };
+      type: DataTypes.INTEGER,
+    },
+  };
 
-  const Order = sequelize.define('Order', OrderSchema, {
-    charset: 'utf8mb4',
-    collate: 'utf8mb4_unicode_ci'
+  const Order = sequelize.define("Order", OrderSchema, {
+    charset: "utf8mb4",
+    collate: "utf8mb4_unicode_ci",
+  });
+
+  Order.sync({ alter: true }).then(() => {
+    console.log("DONE");
   });
 
   return Order;
 };
-
